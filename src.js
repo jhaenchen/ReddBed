cbc5beafd810e17bd8b7af454aa26f106557288a
Jackson Haenchen <jackson_haenchen@gmail.com>
2015-03-17 17:03:47 +0100

    script - removed double reddit post src - added auto update tech

--- src.js
+++ src.js
@@ -1,5 +1,5 @@
 
-var getInfo = function(url,mode,id){
+var getInfo = function(url,mode,element){
 	var request = new XMLHttpRequest();
 	request.open('GET', 'http://www.reddit.com/api/info.json?url='+url, true);
 
@@ -12,15 +12,15 @@ var getInfo = function(url,mode,id){
 		var html = createEmbed(post.title,post.url,post.domain,post.created_utc,post.author,post.subreddit,post.num_comments,post.score,post.thumbnail,post.permalink);
 		switch(mode){
 		case "fill":
-			document.getElementById(id).innerHTML = html;
+			element.innerHTML = html;
 			break;
 		case "prepend":
-			parent.insertBefore(el, parent.firstChild);
+			element.parent.insertBefore(el, parent.firstChild);
 			object.prepend(html);
 			break;
 		}
 		console.log(html);
-		alert("success!");
+		
 	  } else {
 	    // We reached our target server, but it returned an error
 
@@ -33,7 +33,20 @@ var getInfo = function(url,mode,id){
 
 	request.send();
 };
-getInfo("http://en.wikipedia.org/wiki/Alex_%28parrot%29#Accomplishments","fill","reddit");
+
+//Get all the embedded posts and fill them with the right html
+var allPosts = document.getElementsByClassName("reddit_post");
+for(var i = 0; i < allPosts.length; i++){
+	getInfo(allPosts.item(i).getElementsByTagName("a").item(0).href,"fill",allPosts.item(i));
+};
+
+setInterval(function(){
+         var allPosts = document.getElementsByClassName("reddit_post");
+		 for(var i = 0; i < allPosts.length; i++){
+			 console.log(allPosts.item(i).getElementsByClassName("title").item(0).getElementsByTagName("a").item(0).href);
+			getInfo(allPosts.item(i).getElementsByClassName("title").item(0).getElementsByTagName("a").item(0).href,"fill",allPosts.item(i));
+		}
+    },5000);
 
 var createEmbed = function(title,url,domain,date,user,subreddit,commentCount,score,thumbnail,link){
 	return "<div class=\"item\"><div class=\"vote_container\"><div class=\"vote up\"></div><p class=\"votes\">"+score+"</p><div class=\"vote down\"></div></div><div class=\"thumbnail\"><img src=\""+thumbnail+"\"></div><div class=\"details\"><p class=\"title\"><a href=\""+url+"\">"+title+"</a><span style=\"font-size:10px;  color: rgb(136, 136, 136);\">("+domain+")</span></p><p class=\"tagline\">submitted <time title=\""+date+"\" class=\"live-timestamp\">"+timeSince(date)+" ago</time> by <a href=\"http://www.reddit.com/user/"+user+"\">"+user+"</a> to <a href=\"http://www.reddit.com/r/"+subreddit+"/\">/r/"+subreddit+"</a></p><p class=\"comment\"><a href=\"http://www.reddit.com"+link+"\"<b>View "+commentCount+" comments</b></p></div></div>";
@@ -45,19 +58,19 @@ var seconds = Math.floor(((new Date().getTime()/1000) - date)),
 interval = seconds / 31536000;
 var floor = Math.floor(interval);
 if (floor > 1){
-	return floor + " years, "+Math.floor((interval%1)*12)+"months";
+	return floor + " years, "+Math.floor((interval%1)*12)+" months";
 }; 
 
 interval = seconds / 2592000;
 var floor = Math.floor(interval);
 if (floor > 1){
-	return floor + " months, "+Math.floor((interval%1)*30)+"days"; //i know this is bad, but it's a boring bug. someone else can fix it.
+	return floor + " months, "+Math.floor((interval%1)*30)+" days"; //i know this is bad, but it's a boring bug. someone else can fix it.
 };
 
 interval = seconds / 86400;
 var floor = Math.floor(interval);
 if (floor >= 1){
-	return floor + " days, "+Math.floor((interval%1)*24)+"hours";
+	return floor + " days, "+Math.floor((interval%1)*24)+" hours";
 };
 
 interval = seconds / 3600;
