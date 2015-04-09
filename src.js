41437429ba6ccee6f7344fd40d8df00890bde456
Jackson Haenchen <jackson_haenchen@gmail.com>
2015-04-09 13:15:22 +0200

    Removing redundancy, changed some comments, adding css

--- src.js
+++ src.js
@@ -1,4 +1,4 @@
-//This gets the info for a particular reddit comment, parses it, then uses "mode" to determine how to interact with the "element" paramater. Based on "mode" the function will either append or fill.
+//This gets the info for a particular reddit post, parses it, then uses "mode" to determine how to interact with the "element" paramater. Based on "mode" the function will either append or fill.
 var getInfo = function(url,mode,element){
 	var request = new XMLHttpRequest();
 	request.open('GET', 'http://www.reddit.com/api/info.json?url='+url, true);
@@ -10,19 +10,15 @@ var getInfo = function(url,mode,element){
 		console.log(response);
 		var post = response.data.children[0].data;
 		var html = createEmbed(post.title,post.url,post.domain,post.created_utc,post.author,post.subreddit,post.num_comments,post.score,post.thumbnail,post.permalink);
+		var div = document.createElement('div');
+		div.innerHTML = html;
+		var toInsert = div.firstChild;
 		switch(mode){
 		case "fill":
-			var div1 = document.createElement('div');
-			div1.innerHTML = html;
-			var toInsert = div1.firstChild;
 			element.innerHTML = toInsert.innerHTML;
 			break;
 		case "prepend":
 			console.log(element.parentElement);
-			
-			var div = document.createElement('div');
-			div.innerHTML = html;
-			var toInsert = div.firstChild;
 			element.parentElement.insertBefore(toInsert, parent.firstChild);
 			break;
 		}
@@ -134,8 +130,10 @@ if(allSubreddits != null){
 
 //Get all the embedded posts and fill them with the right html
 var allPosts = document.getElementsByClassName("red-item");
+console.log(allPosts);
 if(allPosts != null){
 	for(var i = 0; i < allPosts.length; i++){
+		console.log(allPosts.item(i));
 		getInfo(allPosts.item(i).getElementsByTagName("a").item(0).href,"fill",allPosts.item(i));
 	};
 }
@@ -159,12 +157,12 @@ var styleEl = document.createElement('style'),
   // Grab style sheet
   //styleSheet = styleEl.sheet;
   console.log(styleEl);
-   document.getElementById('reddbed-style').innerHTML = ".red-title { font-family:verdana, arial, helvetica, sans-serif; font-kerning:auto; font-size:16px; font-stretch:normal; font-style:normal; font-variant:normal; font-variant-ligatures:normal; font-weight:400; margin-right:.4em; position:relative; top:px; margin-top:0; margin-bottom:5px; } .red-tagline { font-family:verdana, arial, helvetica, sans-serif; font-kerning:auto; font-size:10px; font-stretch:normal; font-style:normal; font-variant:normal; font-variant-ligatures:normal; font-weight:400; margin-top:-5px; margin-bottom:0; } .red-details { margin-left:135px; } .red-thumbnail,.item { float:left; margin-right:5px; vertical-align:top; } .red-vote { height:14px; width:15px; } .red-up { background-image:url(reddit-images.png); background-position:-63px -818px; background-repeat:no-repeat; bottom-margin:0; height:14px; width:15px; margin:2px auto auto; } .red-down { background-image:url(reddit-images.png); background-position:-21px -818px; background-repeat:no-repeat; bottom-margin:0; height:14px; width:15px; margin:auto; } .red-item a:hover { text-decoration:underline; } .red-item { height:auto; width:100%; -webkit-border-radius:10px; -moz-border-radius:10px; border-radius:10px; border:2px solid #000; background-color:#F0F0F0; padding-left:5px; margin-right:5px; margin-bottom:2px; overflow:hidden; } .red-comment { font-family:verdana, arial, helvetica, sans-serif; font-kerning:auto; font-size:10px; font-stretch:normal; font-style:normal; font-variant:normal; font-variant-ligatures:normal; font-weight:400; margin-top:2px; margin-bottom:2px; } .red-vote_container { width:37px; float:left; height:48px; text-align:center; padding-right:10px; padding-left:3px; } .red-vote_container p { margin-top:1px; margin-bottom:0; font-family:verdana, arial, helvetica, sans-serif; font-kerning:auto; font-size:13px; font-stretch:normal; font-style:normal; font-variant:normal; font-variant-ligatures:normal; font-weight:700; color:#c6c6c6; } .red-item img { width:70px; } .red-item a,.red-title a,.red-title a:hover { text-decoration:none; }";
+   document.getElementById('reddbed-style').innerHTML = ".red-title { font-family: verdana, arial, helvetica, sans-serif; font-kerning: auto; font-size: 16px; font-stretch: normal; font-style: normal; font-variant: normal; font-variant-ligatures: normal; font-weight: 400; margin-right: .4em; position: relative; top: px; margin-top: 0; margin-bottom: 5px; } .red-tagline { font-family: verdana, arial, helvetica, sans-serif; font-kerning: auto; font-size: 10px; font-stretch: normal; font-style: normal; font-variant: normal; font-variant-ligatures: normal; font-weight: 400; margin-top: -5px; margin-bottom: 0; } .red-details { margin-left: 135px; } .red-thumbnail, .item { float: left; margin-right: 5px; vertical-align: top; } .red-vote { height: 14px; width: 15px; } .red-up { background-image: url(reddit-images.png); background-position: -63px -818px; background-repeat: no-repeat; bottom-margin: 0; height: 14px; width: 15px; margin: 2px auto auto; } .red-down { background-image: url(reddit-images.png); background-position: -21px -818px; background-repeat: no-repeat; bottom-margin: 0; height: 14px; width: 15px; margin: auto; } .red-item a:hover { text-decoration: underline; } .red-item { height: auto; width: 100%; -webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; border: 2px solid #000; background-color: #F0F0F0; padding-left: 5px; margin-right: 5px; margin-bottom: 2px; overflow: hidden; } .red-comment-count { font-family: verdana, arial, helvetica, sans-serif; font-kerning: auto; font-size: 10px; font-stretch: normal; font-style: normal; font-variant: normal; font-variant-ligatures: normal; font-weight: 400; margin-top: 2px; margin-bottom: 2px; } .red-vote_container { width: 37px; float: left; height: 48px; text-align: center; padding-right: 10px; padding-left: 3px; } .red-vote_container p { margin-top: 1px; margin-bottom: 0; font-family: verdana, arial, helvetica, sans-serif; font-kerning: auto; font-size: 13px; font-stretch: normal; font-style: normal; font-variant: normal; font-variant-ligatures: normal; font-weight: 700; color: #c6c6c6; } .red-item img { width: 70px; } .red-item a, .red-title a, .red-title a:hover { text-decoration: none; } .child{ margin-left:5px; margin-right:13px; margin-bottom:20px; margin-top:20px; }";
 	//styleEl.innerHTML("body{font-size:12px;}\n.title{font-size:22;}");
 
 //This simply formats a string to take the individual pieces of information for any comment and return the properly formed HTML div.
 	var createEmbed = function(title,url,domain,date,user,subreddit,commentCount,score,thumbnail,link){
-		return "<div class=\"red-item\"><div class=\"red-vote_container\"><div class=\"red-up\"></div><p class=\"red-votes\">"+score+"</p><div class=\"red-down\"></div></div><div class=\"red-thumbnail\"><img src=\""+thumbnail+"\"></div><div class=\"red-details\"><p class=\"red-title\"><a href=\""+url+"\">"+title+"</a><span style=\"font-size:10px;  color: rgb(136, 136, 136);\"> ("+domain+")</span></p><p class=\"red-tagline\">submitted <time title=\""+date+"\" class=\"red-live-timestamp\">"+timeSince(date)+" ago</time> by <a href=\"http://www.reddit.com/user/"+user+"\">"+user+"</a> to <a href=\"http://www.reddit.com/r/"+subreddit+"/\">/r/"+subreddit+"</a></p><p class=\"red-comment\"><a href=\"http://www.reddit.com"+link+"\"<b>View "+commentCount+" comments</b></p></div></div>";
+		return "<div class=\"red-item\"><div class=\"red-vote_container\"><div class=\"red-up\"></div><p class=\"red-votes\">"+score+"</p><div class=\"red-down\"></div></div><div class=\"red-thumbnail\"><img src=\""+thumbnail+"\"></div><div class=\"red-details\"><p class=\"red-title\"><a href=\""+url+"\">"+title+"</a><span style=\"font-size:10px;  color: rgb(136, 136, 136);\"> ("+domain+")</span></p><p class=\"red-tagline\">submitted <time title=\""+date+"\" class=\"red-live-timestamp\">"+timeSince(date)+" ago</time> by <a href=\"http://www.reddit.com/user/"+user+"\">"+user+"</a> to <a href=\"http://www.reddit.com/r/"+subreddit+"/\">/r/"+subreddit+"</a></p><p class=\"red-comment-count\"><a href=\"http://www.reddit.com"+link+"\"<b>View "+commentCount+" comments</b></p></div></div>";
 	};
 
 //This calculates how long it has been in human readable time since a certain date.
